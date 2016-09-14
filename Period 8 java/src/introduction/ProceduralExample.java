1c7c362902d2385565d82bd71f52f0e8a5fef7ba
Student 8 <Student 8@10.8.33.170>
2016-09-14 14:00:53 -0400

    Third!

--- Period 8 java/src/introduction/ProceduralExample.java
+++ Period 8 java/src/introduction/ProceduralExample.java
@@ -3,6 +3,6 @@ package introduction;
 public class ProceduralExample {
   public static void main(String[] args){
   System.out.println("Hello World");
-  System.out.println("2nd Hello");
+  System.out.println("PC");
   }
 }
