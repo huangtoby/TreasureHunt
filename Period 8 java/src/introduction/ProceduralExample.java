ab350301af37daa598863238f705d8c7f417c7ac
acuzzi4520 <acuzzi4520@bths.edu>
2016-09-14 14:00:26 -0400

    Update ProceduralExample.java

--- Period 8 java/src/introduction/ProceduralExample.java
+++ Period 8 java/src/introduction/ProceduralExample.java
@@ -3,6 +3,6 @@ package introduction;
 public class ProceduralExample {
   public static void main(String[] args){
   System.out.println("Hello World 1");
-  System.out.println("Hello World 3");
+  System.out.println("Browser");
   }
 }
