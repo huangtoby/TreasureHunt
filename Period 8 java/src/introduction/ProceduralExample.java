e29515fe7f2633ee53e13c6af73d4fd7b5d895d9
acuzzi4520 <acuzzi4520@bths.edu>
2016-09-15 13:52:56 -0400

    Update ProceduralExample.java

--- Period 8 java/src/introduction/ProceduralExample.java
+++ Period 8 java/src/introduction/ProceduralExample.java
@@ -2,7 +2,9 @@ package introduction;
 
 public class ProceduralExample {
   public static void main(String[] args){
-  System.out.println("Hello World 1");
-  System.out.println("Browser");
+  System.out.println("Hello World");
+  String[] names = {"Jillian","Joseph","Jordan"};
+  String[] years = {"Freshman","Sophomore","Junior"};
+  System.out.println("Hi, my name is" +names[0]+ ", and I am a" +years[0]);
   }
 }
