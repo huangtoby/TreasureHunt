86b5280f1fa8a304ea816bcfd5287df783e0febc
Student 8 <Student 8@10.8.33.170>
2016-09-19 14:02:57 -0400

    Int

--- Period 8 java/src/introduction/DataTypes.java
+++ Period 8 java/src/introduction/DataTypes.java
@@ -3,6 +3,6 @@ package introduction;
 public class DataTypes {
 
 	public static void main(String[] args) {
-		if(42 == 42.0) System.out.println("42 equals 42.0");
+		if(42 == 42.0) System.out.println("42 equals 42.0"):
 	}
 }
