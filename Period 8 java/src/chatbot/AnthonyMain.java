9916ef418a961326aa7db020fd90c199c82bf304
Student 8 <Student 8@10.8.33.170>
2016-09-28 12:37:32 -0400

    update

--- Period 8 java/src/chatbot/AnthonyMain.java
+++ Period 8 java/src/chatbot/AnthonyMain.java
@@ -36,8 +36,8 @@ public class AnthonyMain {
 			
 			//response to how you feel
 			if(findKeyword(response, "good", 0) >= 0){
-				print("That's wonderful, "+user
-						+", So glad you feel good.");
+				print("That's wonderful. "
+						+"So glad you feel good.");
 			
 			}
 			else if(response.indexOf("school") >= 0){
@@ -56,8 +56,12 @@ public class AnthonyMain {
 		//make lowercase
 		searchString = searchString.toLowerCase();
 		keyword = keyword.toLowerCase();
+		System.out.println("The Phrase Is "+ searchString);
+		System.out.println("The Keyword Is "+ keyword);
 		//find first position of key word
-		int psn = searchString.indexOf(0);
+		int psn = searchString.indexOf(keyword);
+		System.out.println("The Keyword was found at "+psn);
+		
 		//keeps searching until context keyword found
 		while(psn >= 0){
 			//assume preceeded and followed by space
@@ -67,6 +71,8 @@ public class AnthonyMain {
 			if(psn > 0){
 				before = 
 						searchString.substring(psn-1, psn);
+				System.out.println("The Character before is "
+						+before);
 			}
 			//check if there is a character
 			//after the keyword
@@ -75,20 +81,69 @@ public class AnthonyMain {
 						searchString.substring(psn + 
 								keyword.length(),
 								psn + keyword.length() + 1);
+				System.out.println("The Character after is "
+						+after);
 			}
 			if (before.compareTo("a") < 0 &&
-					after.compareTo("a") < 0){
+					after.compareTo("a") < 0 &&
+				noNegations(searchString, psn)){
+				System.out.println("Found "+keyword+" at "
+						+psn);
 				return psn;
 			}else{
 				//psn+1 is one space after our current
 				//psn, so this is the next word
 				psn = searchString.indexOf(keyword,psn+1);
+				System.out.println("Did not find "+keyword
+						+", checking position "
+						+psn);
 			}
 		}
 		
 		return -1;
 	}
-	
+	/**
+	 * This is a helper method.A helper method is a method
+	 * designed for "helping" a larger method.Because of this, 
+	 * helper methods are generally private because they are 
+	 * only used by the methods they are helping.Also when 
+	 * you do your project, i expect to see helper methods
+	 * because they also make code more readable
+	 * 
+	 * @param searchString (always lowercase)
+	 * @param psn
+	 * @return "true" if there is no negation words 
+	 * in front of psn
+	 */
+	private static boolean noNegations(String searchString, 
+			int psn) {
+		//check to see if the word no is in front 
+		//of psn
+		//check to see if there are 3 spaces in front
+		//then check to see if "no" is there 
+		if(psn - 3 >= 0 && 
+				searchString.substring(psn-3,psn)
+				.equals("no ")){
+			return false;
+		}
+		if(psn - 4 >= 0 && 
+				searchString.substring(psn-4,psn)
+				.equals("not ")){
+			return false;
+		}
+		if(psn - 6 >= 0 && 
+				searchString.substring(psn-6,psn)
+				.equals("never ")){
+			return false;
+		}
+		if(psn - 4 >= 0 && 
+				searchString.substring(psn-4,psn)
+				.equals("n't ")){
+			return false;
+		}
+		return true;
+	}
+
 	public static String promptInput(){
 		String userInput = input.nextLine();
 		return userInput;
